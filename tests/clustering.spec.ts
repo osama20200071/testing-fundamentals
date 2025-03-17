51ed77e43da0d09ca9f4212de606c66dd4fb93bb
Osama <osamakaram118@gmail.com>
2025-03-17 02:00:11 +0200

    building and reusing our own clusterPage

--- tests/clustering.spec.ts
+++ tests/clustering.spec.ts
@@ -1,16 +1,64 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:5173/clustering');
-  await expect(page).toHaveTitle('Chicago Traffic Accidents Clustering');
+  // await page.goto('http://localhost:5173/clustering');
+  // await expect(page).toHaveTitle('Chicago Traffic Accidents Clustering');
+
+  const clusterPage = new ClusterPage(page);
+  await clusterPage.goto({});
+  await expect(clusterPage.title).resolves.toBe(
+    'Chicago Traffic Accidents Clustering'
+  );
+  // expect(await clusterPage.title).toBe('Chicago Traffic Accidents Clustering');
 });
 
 test('it prints the current Parameters', async ({ page }) => {
-  await page.goto(
-    'http://localhost:5173/clustering/?size=5000&distance=100&minClusterSize=10'
-  );
+  // await page.goto(
+  //   'http://localhost:5173/clustering/?size=5000&distance=100&minClusterSize=10'
+  // );
+  // await expect(page.locator('span.size')).toHaveText('5000');
+  // await expect(page.locator('span.distance')).toHaveText('100');
+  // await expect(page.locator('span.min-cluster-size')).toHaveText('10');
 
-  await expect(page.locator('span.size')).toHaveText('5000');
-  await expect(page.locator('span.distance')).toHaveText('100');
-  await expect(page.locator('span.min-cluster-size')).toHaveText('10');
+  const clusterPage = new ClusterPage(page);
+  await clusterPage.goto({ distance: 100, minClusterSize: 10, size: 5000 });
+  await expect(clusterPage.size).toHaveText('5000');
+  await expect(clusterPage.distance).toHaveText('100');
+  await expect(clusterPage.minClusterSize).toHaveText('10');
 });
+
+type GotoPrams = {
+  distance?: number;
+  size?: number;
+  minClusterSize?: number;
+};
+
+class ClusterPage {
+  constructor(private page: Page) {}
+
+  goto({ distance, minClusterSize, size }: GotoPrams) {
+    const url = new URL('http://localhost:5173/clustering');
+    if (distance) url.searchParams.set('distance', distance.toString());
+    if (size) url.searchParams.set('size', size.toString());
+    if (minClusterSize)
+      url.searchParams.set('minClusterSize', minClusterSize.toString());
+
+    return this.page.goto(url.toString());
+  }
+
+  get title() {
+    return this.page.title();
+  }
+
+  get distance() {
+    return this.page.locator('span.distance');
+  }
+
+  get size() {
+    return this.page.locator('span.size');
+  }
+
+  get minClusterSize() {
+    return this.page.locator('span.min-cluster-size');
+  }
+}
